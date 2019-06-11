ebdaf82626bffcf313fc71c98293fd95f160307f
RakeshKrishna143 <41262164+RakeshKrishna143@users.noreply.github.com>
2019-06-11 09:29:10 +0530

    Create fibonacci_element.java
    This program provides the nth element in the fibonacci series

---
+++ fibonacci_element.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class FibonacciElement {
+	public static void main(String args[]) {
+		System.out.print("enter number of elements:");
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int n1=0,n2=1,n3;
+		for(int i=1;i<=n;i++) {
+			if(i==1) {
+				n3=n1;
+			}
+			else if(i==2){
+				n3=n2;
+			}
+			else {
+				n3=n1+n2;
+				n1=n2;
+				n2=n3;
+			}
+		
+		}
+		System.out.println(n2);
+		
+		
+	}
+
+}
