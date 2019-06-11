d0234285e825081d9e0f3e5d49c32b205674b1f1
RakeshKrishna143 <41262164+RakeshKrishna143@users.noreply.github.com>
2019-06-11 22:43:58 +0530

    Create Find_Duplicate_Char.java

---
+++ Find_Duplicate_Char.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class FindDuplicateChar {
+	public static void main(String args[]) {
+		String s;
+		Scanner sc = new Scanner(System.in);
+		s=sc.nextLine();
+		FindDuplicateChar output = new FindDuplicateChar(s);
+	}
+	FindDuplicateChar(String s){
+		Map<Character,Integer> map=new HashMap<Character,Integer>();
+		char ch[]=s.toCharArray();
+		for (char c:ch) {
+			if(map.containsKey(c)) {
+				map.put(c,map.get(c)+1);
+			}
+			else {
+				map.put(c,1);
+			}
+		}
+		Set< Character> keys = map.keySet();
+		for( char c:keys) {
+			if(map.get(c)>1) {
+				System.out.println(c+""+map.get(c));
+			}
+		}
+		
+		
+	}
+
+}
